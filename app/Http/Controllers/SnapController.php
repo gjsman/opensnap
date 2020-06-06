90ea839d0ab292d64d92de59512b9b108cbe8c8b
Gabriel Sieben <g.j.sieben@outlook.com>
2020-06-06 11:43:05 -0700

    v2_snaps_find API passthrough to Snap Store

--- app/Http/Controllers/SnapController.php
+++ app/Http/Controllers/SnapController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Resources\SnapCollection;
-
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\URL;
 
 class SnapController extends Controller
 {
@@ -62,6 +63,12 @@ class SnapController extends Controller
     }
 
     public function v2_snaps_find(Request $request) {
+        /** 
+         * Finds a list of snaps on the opensnap server, followed by snaps on the official Snap Store.
+         * If a snap is found on the local server whose name conflicts with the official Snap Store,
+         * the OpenSnap server wins and the Snap Store app is not displayed. This behavior may change
+         * in a future version.
+         */
         $response = [
             "results" => [
 
@@ -69,11 +76,13 @@ class SnapController extends Controller
         ];
         $snaps = \App\Snap::where('name', 'like', "%".$request->get('q')."%")->get();
         $snap_iteration = 0;
+        $snap_names = array();
 
         foreach ($snaps as $snap) {
+            $snap_names[] = $snap["name"];
             $snap_response = &$response["results"][$snap_iteration];
 
-            $snap_response["name"] = $snap["name"];
+            $snap_response["name"] = $snap["name"]." [opensnap]";
             $snap_response["snap"] = [
                 "contact" => "",
                 "description" => "not implemented",
@@ -85,6 +94,21 @@ class SnapController extends Controller
                 "summary" => $snap["summary"],
                 "title" => $snap["title"],
                 "website" => "not implemented",
+            ];
+
+            $revision = $snap->Channels()->get()->first();
+            $download = $revision->Download()->get()->first();
+            $snap_response["revision"] = [
+                "base" => null,
+                "channel" => $revision["risk"],
+                "revision" => $revision["revision"],
+                // TODO: Add to DB as option
+                "confinement" => "classic",
+                "type" => $revision["type"],
+                "version" => $revision["version"],
+                "download" => [
+                    "size" => $download["size"],
+                ],
             ];
 
             $publisher = $snap->Publisher()->get()->first();
@@ -99,7 +123,28 @@ class SnapController extends Controller
 
             $snap_iteration += 1;
         }
+        
+        $passthrough = new Client();
+        $url = Url::full();
+        $url = str_replace(Url::to('/')."/api", "https://api.snapcraft.io", $url);
+        $passthrough_response = $passthrough->request('GET', $url, [
+            "headers" => [
+                "Snap-Device-Series" => "16",
+            ],
+        ]);
 
+        $snaps_passthrough = json_decode($passthrough_response->getBody()->getContents(), true);
+        $snap_iteration = count($response) + 1;
+        foreach ($snaps_passthrough["results"] as $snap) {
+            // The prices field isn't an array, it's an object. Weird, as my PHP tool
+            // will automatically convert it to a JSON array for me, but this causes
+            // snap to crash. Needs to be an empty object.
+            $snap["snap"]["prices"] = (object) null;
+            if (!(in_array($snap["name"], $snap_names))) {
+                $response["results"][] = $snap;
+            }
+            $snap_iteration += 1;
+        }
         return response()->json($response);
     }
 }
