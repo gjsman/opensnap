baef6007ec96b9f48253d903218cd3598acdfd49
Gabriel Sieben <g.j.sieben@outlook.com>
2020-06-06 10:28:31 -0700

    Second API: v2_snaps_find

--- app/Http/Controllers/SnapController.php
+++ app/Http/Controllers/SnapController.php
@@ -60,4 +60,46 @@ class SnapController extends Controller
 
         return response()->json($response);
     }
+
+    public function v2_snaps_find(Request $request) {
+        $response = [
+            "results" => [
+
+            ],
+        ];
+        $snaps = \App\Snap::where('name', 'like', "%".$request->get('q')."%")->get();
+        $snap_iteration = 0;
+
+        foreach ($snaps as $snap) {
+            $snap_response = &$response["results"][$snap_iteration];
+
+            $snap_response["name"] = $snap["name"];
+            $snap_response["snap"] = [
+                "contact" => "",
+                "description" => "not implemented",
+                "license" => $snap["license"],
+                "media" => [],
+                "publisher" => [],
+                "private" => false,
+                "store-url" => $snap["store-url"],
+                "summary" => $snap["summary"],
+                "title" => $snap["title"],
+                "website" => "not implemented",
+            ];
+
+            $publisher = $snap->Publisher()->get()->first();
+            $snap_response["snap"]["publisher"] = [
+                "display-name" => $publisher["display-name"],
+                "publisher-id" => $publisher["publisher-id"],
+                "username" => $publisher["username"],
+                "validation" => $publisher["validation"],
+            ];
+
+            $snap_response["snap-id"] = $snap["snap-id"];
+
+            $snap_iteration += 1;
+        }
+
+        return response()->json($response);
+    }
 }
